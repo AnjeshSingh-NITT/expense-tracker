e10483b167385c52132015ec6614573dd21045e1
Anjesh Singh <singhanjesh2005@gmail.com>
2024-09-22 01:49:36 +0530

    Setting up Firebase authentication

---
+++ src/config/firebase-config.js
@@ -0,0 +1,21 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDXiHKehzd9RdUudj5HEdUm7b-cXWVEV6c",
+  authDomain: "expense-tracker-eec5b.firebaseapp.com",
+  projectId: "expense-tracker-eec5b",
+  storageBucket: "expense-tracker-eec5b.appspot.com",
+  messagingSenderId: "947425785187",
+  appId: "1:947425785187:web:61bda9da7edfab72ee8f56"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+//firebase login
+//firebase init
+//firebase deploy
